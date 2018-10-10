433835b8d16fb407e25b5ed9ce94f9e833885b6a
Ben Drucker <bvdrucker@gmail.com>
2018-10-10 11:04:43 -0700

    Support multiple values in proxy headers (#2)
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
    https://en.wikipedia.org/wiki/X-Forwarded-For#Format

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ function ReqIp (header) {
   header = header || 'x-forwarded-for'
 
   return function ip (req, res, callback) {
-    req.ip = req.headers[header] || req.connection.remoteAddress
+    req.ip = proxyHeader(req, header) || req.connection.remoteAddress
     callback()
   }
+}
+
+function proxyHeader (req, header) {
+  const value = req.headers[header]
+  return value && value.split(', ')[0]
 }
