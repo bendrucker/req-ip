45ecd035855405dbf52e1a80e880efb1542ce39c
Ben Drucker <bvdrucker@gmail.com>
2018-10-10 10:12:43 -0700

    Support multiple values in proxy headers
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
    https://en.wikipedia.org/wiki/X-Forwarded-For#Format

--- test.js
+++ test.js
@@ -19,7 +19,27 @@ test('default header', function (t) {
   function dispatch (req, res) {
     ip(req, res, function (err) {
       if (err) return t.end(err)
-      t.equal(req.ip, '1.2.3.4')
+      t.equal(req.ip, '1.2.3.4', 'uses x-forwarded-for header')
+    })
+  }
+})
+
+test('multiple proxies', function (t) {
+  t.plan(1)
+
+  const ip = ReqIp()
+
+  inject(dispatch, {
+    url: '/',
+    headers: {
+      'x-forwarded-for': '1.2.3.4, 5.6.7.8'
+    }
+  }, t.fail)
+
+  function dispatch (req, res) {
+    ip(req, res, function (err) {
+      if (err) return t.end(err)
+      t.equal(req.ip, '1.2.3.4', 'uses first x-forwarded-for entry')
     })
   }
 })
@@ -39,7 +59,7 @@ test('custom header', function (t) {
   function dispatch (req, res) {
     ip(req, res, function (err) {
       if (err) return t.end(err)
-      t.equal(req.ip, '10.20.30.40')
+      t.equal(req.ip, '10.20.30.40', 'uses custom header')
     })
   }
 })
@@ -56,7 +76,7 @@ test('not forwarded', function (t) {
   function dispatch (req, res) {
     ip(req, res, function (err) {
       if (err) return t.end(err)
-      t.equal(req.ip, '127.0.0.1')
+      t.equal(req.ip, '127.0.0.1', 'falls back to remoteAddress')
     })
   }
 })
